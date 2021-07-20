36cd98022108ea07b4317013b63d5ba609ff7d66
Christian Eland <eland.christian@gmail.com>
2021-07-19 22:31:08 -0300

    doc(XmlValidator): add docstring to class

--- src/domain/adapters/XmlValidatorAdapter.ts
+++ src/domain/adapters/XmlValidatorAdapter.ts
@@ -2,6 +2,9 @@ import { IValidator } from '../protocols/IValidator'
 import { XmlMetadata } from '../value-objects/XmlMetadata'
 import { validate } from 'fast-xml-parser'
 
+/**
+ * Adapt to external fast-xml-parser module
+ */
 export class XmlValidatorAdapter implements IValidator {
   public isValid (xmlMetadata: XmlMetadata): boolean {
     if (validate(xmlMetadata.props.xml) === true) {
