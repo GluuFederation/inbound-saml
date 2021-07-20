dca2b495149721a75475aea0224b2482a61bf444
Christian Eland <eland.christian@gmail.com>
2021-07-19 22:24:55 -0300

    feat(XmlValidator): ensure `isValid` return false if validator returns Error

--- src/domain/adapters/XmlValidatorAdapter.spec.ts
+++ src/domain/adapters/XmlValidatorAdapter.spec.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from 'fs'
-import xml2js from 'xml2js'
+import * as parser from 'fast-xml-parser'
+import fs from 'fs'
 import { IValidator } from '../protocols/IValidator'
 import { XmlMetadata, XmlMetadataProps } from '../value-objects/XmlMetadata'
 import { XmlValidatorAdapter } from './XmlValidatorAdapter'
@@ -9,7 +9,7 @@ const validXmlFilePath = process.cwd() + '/src/testdata/shibIdpMetadata.xml'
 // const validXmlString = readFileSync(validXmlFilePath).toString()
 
 const validXmlString = '<root>Any valid xml</root>'
-
+// const invalidXml = '<asda> Invalid xml >'
 const fakeXmlProps: XmlMetadataProps = {
   xml: validXmlString
 }
@@ -29,18 +29,38 @@ const fakeXmlMetadata = (xmlValidatorStub: IValidator): XmlMetadata => {
   return fakeXmlMetadata
 }
 
+jest.mock('fast-xml-parser')
+
 describe('XmlValidatorAdapter', () => {
-  it('should call xml parser with correct value', () => {
-    const parseStringSpy = jest.spyOn(xml2js, 'parseString')
+  it('should call xml validate with correct value', () => {
+    const validateSpy = jest.spyOn(parser, 'validate')
     const sut = new XmlValidatorAdapter()
     const fakeXmlValidator = makeFakeValidator()
 
     // call isValid with a different XmlValidator, to avoid redundance
     sut.isValid(fakeXmlMetadata(fakeXmlValidator))
+    expect(validateSpy).toHaveBeenCalledTimes(1)
+    expect(validateSpy).toHaveBeenCalledWith(validXmlString)
+  })
+  it('should return false if validate returns ValidationError', () => {
+    interface ValidationError {
+      err: { code: string, msg: string, line: number }
+    }
+    /**
+     * @implements private ValidationError
+     */
+    const validationError: ValidationError = {
+      err: {
+        code: 'code1',
+        msg: 'error message',
+        line: 9
+      }
+    }
+    const validateSpy = jest.spyOn(parser, 'validate').mockReturnValueOnce(validationError)
+    const sut = new XmlValidatorAdapter()
+    const fakeXmlValidator = makeFakeValidator()
 
-    expect(parseStringSpy).toHaveBeenCalledTimes(1)
-
-    // assert first argument
-    expect(parseStringSpy.mock.calls[0][0]).toEqual(validXmlString)
+    // call isValid with a different XmlValidator, to avoid redundance
+    expect(sut.isValid(fakeXmlMetadata(fakeXmlValidator))).toBeFalsy()
   })
 })
--- src/domain/adapters/XmlValidatorAdapter.ts
+++ src/domain/adapters/XmlValidatorAdapter.ts
@@ -1,18 +1,12 @@
 import { IValidator } from '../protocols/IValidator'
 import { XmlMetadata } from '../value-objects/XmlMetadata'
-import { parseString } from 'xml2js'
-import { parse } from 'uuid'
+import * as parser from 'fast-xml-parser'
 
 export class XmlValidatorAdapter implements IValidator {
   public isValid (xmlMetadata: XmlMetadata): boolean {
-    parseString(xmlMetadata.props.xml, (err, result) => {
-      if (err !== undefined) {
-        //
-      }
-      if (result !== undefined) {
-        //
-      }
-    })
-    return true
+    if (parser.validate(xmlMetadata.props.xml) === true) {
+      // implement
+    }
+    return false
   }
 }
