1b28d96816d043061d98e47899aa45248a6747e3
Christian Eland <eland.christian@gmail.com>
2021-07-19 23:12:08 -0300

    test(integration): ensure xml adapter returns false if invalid xml

---
+++ src/domain/adapters/XmlValidatorAdapter.test.ts
@@ -0,0 +1,35 @@
+
+import { readFileSync } from 'fs'
+import { IValidator } from '../protocols/IValidator'
+import { XmlMetadata, XmlMetadataProps } from '../value-objects/XmlMetadata'
+import { XmlValidatorAdapter } from './XmlValidatorAdapter'
+const validXmlFilePath = process.cwd() + '/src/testdata/shibIdpMetadata.xml'
+
+const validXmlString = readFileSync(validXmlFilePath).toString()
+
+const fakeXmlProps: XmlMetadataProps = {
+  xml: validXmlString
+}
+
+const makeFakeValidator = (): IValidator => {
+  class FakeXmlValidator implements IValidator {
+    isValid (data: XmlMetadata): boolean {
+      return true
+    }
+  }
+  return new FakeXmlValidator()
+}
+
+const fakeXmlMetadata = (xmlValidatorStub: IValidator): XmlMetadata => {
+  const xmlValidator = xmlValidatorStub
+  const fakeXmlMetadata = new XmlMetadata(xmlValidator, fakeXmlProps)
+  return fakeXmlMetadata
+}
+
+describe('XmlValidatorAdapter', () => {
+  it('should return true if valid XML is sent as param', () => {
+    const xmlMetadata = fakeXmlMetadata(makeFakeValidator())
+    const sut = new XmlValidatorAdapter()
+    expect(sut.isValid(xmlMetadata)).toBeTruthy()
+  })
+})
