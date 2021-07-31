d58c2636316df910812440c4774d32faeef8a702
Christian Eland <eland.christian@gmail.com>
2021-07-31 00:18:17 -0300

    feat(GetExternalData): ensure ExternalDataMapper is called

--- src/get-saml-metadata/src/application/interactors/GetExternalDataInteractor.spec.ts
+++ src/get-saml-metadata/src/application/interactors/GetExternalDataInteractor.spec.ts
@@ -1,14 +1,21 @@
 // recebe o request com o urlOrPath
 // ele pede pro XmlService carregar o XML
-// ele parsa o Xml pra IdpMetadata
+// ele parsa o Xml pra objeto que impl IMetadata
+// ele adapta para IExternalData
 // ele gera o RespondeModel com IdpMetadata
 
 // receive request model
 // export class urlOrPath extends ValueObject<interface {a: string}>
 
+import { fakeMetadata, validMetadataString, validXmlMetadata } from '../../../../testdata/fakes'
+import { makeXmlMetadata } from '../../domain/factories/makeXmlMetadata'
 import { IValidator } from '../../domain/protocols/IValidator'
-import { IXmlMetadataLoadService } from '../../domain/services/protocols/IXmlMetadataLoadService'
+import { IXmlMetadataLoaderRepository } from '../../domain/utils/IXmlMetadataLoaderRepository'
 import { XmlMetadata } from '../../domain/value-objects/XmlMetadata'
+import { IExternalData } from '../protocols/IExternalData'
+import { IExternalDataMapper } from '../protocols/IExternalDataMapper'
+import { IMetadataMapper } from '../protocols/IMetadataMapper'
+import { IMetadata } from '../protocols/IMetadataTypes'
 import { GetExternalDataInteractor } from './GetExternalDataInteractor'
 import { GetExternalDataRequestModel } from './GetExternalDataRequestModel'
 import { IGetExternalDataInputBoundary } from './IGetExternalDataInputBoundary'
@@ -26,28 +33,60 @@ const makeValidator = (): IValidator => {
   return new ValidatorStub()
 }
 
-const makeXmlMetadataLoadService = (): IXmlMetadataLoadService => {
-  const validator = makeValidator()
-  const xml = 'valid xml'
-  class XmlMetadataLoadServiceStub implements IXmlMetadataLoadService {
-    load (): XmlMetadata {
-      return new XmlMetadata(validator, { xml })
+const makeXmlMetadataLoader = (): IXmlMetadataLoaderRepository => {
+  class XmlMetadataLoaderStub implements IXmlMetadataLoaderRepository {
+    load (urlOrPath: string): XmlMetadata {
+      return makeXmlMetadata({ xml: validMetadataString })
     }
   }
-  return new XmlMetadataLoadServiceStub()
+  return new XmlMetadataLoaderStub()
+}
+
+const makeMetadataMapper = (): IMetadataMapper => {
+  class MetadataMapperStub implements IMetadataMapper {
+    map (xmlData: string): IMetadata {
+      return fakeMetadata
+    }
+  }
+  return new MetadataMapperStub()
+}
+
+const makeExternalDataMapper = (): IExternalDataMapper => {
+  class ExternalDataMapperStub implements IExternalDataMapper {
+    map (metadata: IMetadata): IExternalData {
+      return {
+        idpSigningCert: ['valid cert 1', 'valid cert 2'],
+        singleSignOnServices: [
+          {
+            binding: 'valid binding',
+            location: 'valid location'
+          }
+        ]
+      }
+    }
+  }
+  return new ExternalDataMapperStub()
 }
 
 interface SutTypes {
   sut: IGetExternalDataInputBoundary
-  xmlMetadataLoadServiceStub: IXmlMetadataLoadService
+  xmlMetadataLoaderStub: IXmlMetadataLoaderRepository
+  metadataMapperStub: IMetadataMapper
+  externalDataMapperStub: IExternalDataMapper
 }
 
 const makeSut = (): SutTypes => {
-  const xmlMetadataLoadServiceStub = makeXmlMetadataLoadService()
-  const sut = new GetExternalDataInteractor(xmlMetadataLoadServiceStub)
+  const metadataMapperStub = makeMetadataMapper()
+  const xmlMetadataLoaderStub = makeXmlMetadataLoader()
+  const externalDataMapperStub = makeExternalDataMapper()
+  const sut = new GetExternalDataInteractor(
+    xmlMetadataLoaderStub, metadataMapperStub, externalDataMapperStub
+  )
   return {
     sut,
-    xmlMetadataLoadServiceStub
+    xmlMetadataLoaderStub,
+    metadataMapperStub,
+    externalDataMapperStub
   }
 }
 
@@ -62,12 +101,28 @@ describe('GetExternalDataInteractor', () => {
       const { sut } = makeSut()
       expect(await sut.execute(fakeRequestModel)).toBeUndefined()
     })
-    it('should call service load with urlOrPath', async () => {
-      const { sut, xmlMetadataLoadServiceStub } = makeSut()
-      const xmlMetadataLoadServiceSpy = jest.spyOn(xmlMetadataLoadServiceStub, 'load')
+    it('should call loader with urlOrPath', async () => {
+      const { sut, xmlMetadataLoaderStub } = makeSut()
+      const loadSpy = jest.spyOn(xmlMetadataLoaderStub, 'load')
       await sut.execute(fakeRequestModel)
-      expect(xmlMetadataLoadServiceSpy).toBeCalledTimes(1)
-      // expect(await sut.execute(fakeRequestModel))
+      expect(loadSpy).toBeCalledTimes(1)
+      expect(loadSpy).toHaveBeenCalledWith(fakeRequestModel.urlOrPath)
+    })
+    it('should call MetadataMapper.map', async () => {
+      const { sut, metadataMapperStub, xmlMetadataLoaderStub } = makeSut()
+      jest.spyOn(xmlMetadataLoaderStub, 'load')
+        .mockReturnValueOnce(validXmlMetadata)
+      const mapSpy = jest.spyOn(metadataMapperStub, 'map')
+      await sut.execute(fakeRequestModel)
+      expect(mapSpy).toHaveBeenCalledWith(validXmlMetadata.props.xml)
+    })
+    it('should call ExternalData.map with mapped metadata', async () => {
+      const { sut, metadataMapperStub, externalDataMapperStub } = makeSut()
+      jest.spyOn(metadataMapperStub, 'map').mockReturnValue(fakeMetadata)
+      const mapSpy = jest.spyOn(externalDataMapperStub, 'map')
+      await sut.execute(fakeRequestModel)
+      expect(mapSpy).toHaveBeenCalledTimes(1)
+      expect(mapSpy).toHaveBeenCalledWith(fakeMetadata)
     })
   })
 })
--- src/get-saml-metadata/src/application/interactors/GetExternalDataInteractor.ts
+++ src/get-saml-metadata/src/application/interactors/GetExternalDataInteractor.ts
@@ -1,16 +1,29 @@
-import { IXmlMetadataLoadService } from '../../domain/services/protocols/IXmlMetadataLoadService'
+import { IXmlMetadataLoaderRepository } from '../../domain/utils/IXmlMetadataLoaderRepository'
+import { IExternalDataMapper } from '../protocols/IExternalDataMapper'
+import { IMetadataMapper } from '../protocols/IMetadataMapper'
 import { GetExternalDataRequestModel } from './GetExternalDataRequestModel'
 import { IGetExternalDataInputBoundary } from './IGetExternalDataInputBoundary'
 
 export class GetExternalDataInteractor implements IGetExternalDataInputBoundary {
-  private readonly xmlMetadataLoadService: IXmlMetadataLoadService
+  private readonly xmlMetadataLoader: IXmlMetadataLoaderRepository
+  private readonly metadataMapper: IMetadataMapper
+  private readonly externalDataMapper: IExternalDataMapper
+
   constructor (
-    xmlMetadataLoadService: IXmlMetadataLoadService
+    xmlMetadataLoader: IXmlMetadataLoaderRepository,
+    metadataMapper: IMetadataMapper,
+    externalDataMapper: IExternalDataMapper
   ) {
-    this.xmlMetadataLoadService = xmlMetadataLoadService
+    this.xmlMetadataLoader = xmlMetadataLoader
+    this.metadataMapper = metadataMapper
+    this.externalDataMapper = externalDataMapper
   }
 
   async execute (request: GetExternalDataRequestModel): Promise<void> {
-    this.xmlMetadataLoadService.load()
+    this.externalDataMapper.map(
+      this.metadataMapper.map(
+        this.xmlMetadataLoader.load(request.urlOrPath).props.xml
+      )
+    )
   }
 }
