0d8198b98f60dad303ca3fae903f9ff0b3ed5df2
Christian Eland <eland.christian@gmail.com>
2021-07-31 15:15:58 -0300

    doc(FileValidatorAdapter): add docstring to class

--- src/get-saml-metadata/src/data/adapters/FileValidatorAdapter.ts
+++ src/get-saml-metadata/src/data/adapters/FileValidatorAdapter.ts
@@ -1,6 +1,9 @@
 import { existsSync } from 'fs'
 import { BaseFileValidator } from '../../use-cases/ports/BaseFileValidator'
 
+/**
+ * Check if file exists
+ */
 export class FileValidatorAdapter extends BaseFileValidator {
   isValid (filepath: string): boolean {
     return existsSync(filepath)
