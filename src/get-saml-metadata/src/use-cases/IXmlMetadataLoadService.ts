4f88b9bab08ff3dfce94b4f2c81e391935d1cb51
Christian Eland <eland.christian@gmail.com>
2021-08-01 01:57:42 -0300

    refactor: remove strategy pattern

--- src/get-saml-metadata/src/use-cases/IXmlMetadataLoadService.ts
+++
@@ -1,7 +0,0 @@
-import { XmlMetadata } from '../../../entities/value-objects/XmlMetadata'
-
-export interface IXmlMetadataLoadService {
-  // readonly urlOrPath: string
-  // readonly loader: IXmlMetadataLoaderGateway
-  load: () => XmlMetadata
-}
