930850b9cf3a49b7ff1c024d5601d830d2ee2787
Christian Eland <eland.christian@gmail.com>
2021-08-01 03:26:01 -0300

    feat: ensure mapper is called from controller

--- src/get-saml-metadata/src/interface-adapters/delivery/GetExternalDataController.spec.ts
+++ src/get-saml-metadata/src/interface-adapters/delivery/GetExternalDataController.spec.ts
@@ -6,11 +6,24 @@
 
 import { GetExternalDataRequestModel } from '../../use-cases/GetExternalDataRequestModel'
 import { IGetExternalDataInputBoundary } from '../../use-cases/IGetExternalDataInputBoundary'
+import { IRequestModel } from '../../use-cases/IRequestModel'
 import { IValidator } from '../../use-cases/ports/IValidator'
 import { InvalidPathOrUrlError } from './errors/InvalidPathOrUrlError'
 import { GetExternalDataController } from './GetExternalDataController'
 import { IGetExternalDataRequest } from './protocols/IGetExternalDataRequest'
 import { IRequest } from './protocols/IRequest'
+import { IRequestMapper } from './protocols/IRequestMapper'
+
+const makeMapper = (): IRequestMapper => {
+  class RequestMapperStub implements IRequestMapper {
+    map (request: IRequest<any>): IRequestModel {
+      return {
+        requestId: 'valid id'
+      }
+    }
+  }
+  return new RequestMapperStub()
+}
 
 const makeRequestValidator = (): IValidator => {
   class RequestvalidatorStub implements IValidator {
@@ -33,19 +46,23 @@ interface sutType {
   sut: GetExternalDataController
   externalDataInteractorStub: IGetExternalDataInputBoundary
   requestValidatorStub: IValidator
+  requestMapperStub: IRequestMapper
 }
 
 const makeSut = (): sutType => {
   const requestValidatorStub = makeRequestValidator()
   const externalDataInteractorStub = makeGetExternalDataInteractor()
+  const requestMapperStub = makeMapper()
   const sut = new GetExternalDataController(
     externalDataInteractorStub,
-    requestValidatorStub
+    requestValidatorStub,
+    requestMapperStub
   )
   return {
     sut,
     externalDataInteractorStub,
-    requestValidatorStub
+    requestValidatorStub,
+    requestMapperStub
   }
 }
 
@@ -72,5 +89,11 @@ describe('GetExternalDataController', () => {
     await expect(promise).rejects.toThrow(new InvalidPathOrUrlError(
       validRequest.request.urlOrPath
     ))
+  })
+  it('should call map with request object', async () => {
+    const { sut, requestMapperStub } = makeSut()
+    const mapSpy = jest.spyOn(requestMapperStub, 'map')
+    await sut.handle(validRequest)
+    expect(mapSpy).toHaveBeenCalledWith(validRequest)
   })
 })
--- src/get-saml-metadata/src/interface-adapters/delivery/GetExternalDataController.ts
+++ src/get-saml-metadata/src/interface-adapters/delivery/GetExternalDataController.ts
@@ -4,16 +4,19 @@ import { InvalidPathOrUrlError } from './errors/InvalidPathOrUrlError'
 import { IController } from './protocols/IController'
 import { IGetExternalDataRequest } from './protocols/IGetExternalDataRequest'
 import { IRequest } from './protocols/IRequest'
+import { IRequestMapper } from './protocols/IRequestMapper'
 
 export class GetExternalDataController implements IController {
   constructor (
     private readonly inputChannel: IGetExternalDataInputBoundary,
-    private readonly urlOrPathValidator: IValidator
+    private readonly urlOrPathValidator: IValidator,
+    private readonly requestMapper: IRequestMapper
   ) {}
 
   async handle (request: IRequest<IGetExternalDataRequest>): Promise<void> {
     if (!this.urlOrPathValidator.isValid(request.request.urlOrPath)) {
       throw new InvalidPathOrUrlError(request.request.urlOrPath)
     }
+    this.requestMapper.map(request)
   }
 }
