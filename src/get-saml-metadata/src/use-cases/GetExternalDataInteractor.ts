f16ee2fbcd2c43ba0e3e2e4e267da19302de5055
Christian Eland <eland.christian@gmail.com>
2021-08-02 20:30:07 -0300

    refactor: add abstraction to Interactor

--- src/get-saml-metadata/src/use-cases/GetExternalDataInteractor.ts
+++ src/get-saml-metadata/src/use-cases/GetExternalDataInteractor.ts
@@ -1,34 +1,7 @@
 import { GetExternalDataRequestModel } from './GetExternalDataRequestModel'
-import { IGetExternalDataInputBoundary } from './IGetExternalDataInputBoundary'
-import { IGetExternalDataOutputBoundary } from './IGetExternalDataOutputBoundary'
-import { IExternalDataMapper } from './ports/IExternalDataMapper'
-import { IMetadataMapper } from './ports/IMetadataMapper'
-import { IXmlMetadataLoaderGateway } from './ports/IXmlMetadataLoaderGateway'
+import { BaseGetExternalDataInteractor, IGetExternalDataInputBoundary } from './IGetExternalDataInputBoundary'
 
-export class GetExternalDataInteractor implements IGetExternalDataInputBoundary {
-  private readonly xmlMetadataLoader: IXmlMetadataLoaderGateway
-  private readonly metadataMapper: IMetadataMapper
-  private readonly externalDataMapper: IExternalDataMapper
-  private readonly presenter: IGetExternalDataOutputBoundary
-
-  /**
-   * @param xmlMetadataLoader Loads metadata from file or url...
-   * @param metadataMapper maps xmldata to IMetatada
-   * @param externalDataMapper  maps IMetadata to IExternalData
-   * @param presenter presents output
-   */
-  constructor (
-    xmlMetadataLoader: IXmlMetadataLoaderGateway,
-    metadataMapper: IMetadataMapper,
-    externalDataMapper: IExternalDataMapper,
-    presenter: IGetExternalDataOutputBoundary
-  ) {
-    this.xmlMetadataLoader = xmlMetadataLoader
-    this.metadataMapper = metadataMapper
-    this.externalDataMapper = externalDataMapper
-    this.presenter = presenter
-  }
-
+export class GetExternalDataInteractor extends BaseGetExternalDataInteractor implements IGetExternalDataInputBoundary {
   async execute (request: GetExternalDataRequestModel): Promise<void> {
     const externalData = this.externalDataMapper.map(
       this.metadataMapper.map(
--- src/get-saml-metadata/src/use-cases/IGetExternalDataInputBoundary.ts
+++ src/get-saml-metadata/src/use-cases/IGetExternalDataInputBoundary.ts
@@ -1,5 +1,20 @@
 import { GetExternalDataRequestModel } from './GetExternalDataRequestModel'
+import { IGetExternalDataOutputBoundary } from './IGetExternalDataOutputBoundary'
+import { IExternalDataMapper } from './ports/IExternalDataMapper'
+import { IMetadataMapper } from './ports/IMetadataMapper'
+import { IXmlMetadataLoaderGateway } from './ports/IXmlMetadataLoaderGateway'
 
 export interface IGetExternalDataInputBoundary{
   execute: (request: GetExternalDataRequestModel) => Promise<void>
+}
+
+export abstract class BaseGetExternalDataInteractor implements IGetExternalDataInputBoundary {
+  constructor (
+    protected readonly xmlMetadataLoader: IXmlMetadataLoaderGateway,
+    protected readonly metadataMapper: IMetadataMapper,
+    protected readonly externalDataMapper: IExternalDataMapper,
+    protected readonly presenter: IGetExternalDataOutputBoundary
+  ) {}
+
+  abstract execute (request: GetExternalDataRequestModel): Promise<void>
 }
