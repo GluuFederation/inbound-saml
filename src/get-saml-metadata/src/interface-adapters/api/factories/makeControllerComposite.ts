d70915648926cbc0d2fd61ad196329c5691930b8
Christian Eland <eland.christian@gmail.com>
2021-08-05 00:48:21 -0300

    feat(lib): export isolated getFromFile method

---
+++ src/get-saml-metadata/src/interface-adapters/api/factories/makeControllerComposite.ts
@@ -0,0 +1,25 @@
+import { makeFileValidatorAdapter } from '@get-saml-metadata/interface-adapters/delivery/factories/makeFileValidatorAdapter'
+import { makeGetExternalDataInteractor } from '@get-saml-metadata/interface-adapters/delivery/factories/makeGetExternalDataInteractor'
+import { makeGetExternalDataPresenter } from '@get-saml-metadata/interface-adapters/delivery/factories/makeGetExternalDataPresenter'
+import { makeGetExternalDataRequestMapper } from '@get-saml-metadata/interface-adapters/delivery/factories/makeGetExternalDataRequestMapper'
+import { GetExternalDataController } from '@get-saml-metadata/interface-adapters/delivery/GetExternalDataController'
+import { IController } from '@get-saml-metadata/interface-adapters/delivery/protocols/IController'
+import { EventEmitter } from 'stream'
+
+/**
+ * Creates full composite from presenter to controller
+ * @param eventBus
+ * @returns controller
+ */
+export const makeControllerComposite = (eventBus: EventEmitter): IController => {
+  const presenter = makeGetExternalDataPresenter(eventBus)
+  const interactor = makeGetExternalDataInteractor(presenter)
+  const fileValidator = makeFileValidatorAdapter()
+  const externalRequestMapper = makeGetExternalDataRequestMapper()
+  const controller = new GetExternalDataController(
+    interactor,
+    fileValidator,
+    externalRequestMapper
+  )
+  return controller
+}
--- src/get-saml-metadata/src/lib/index.ts
+++ src/get-saml-metadata/src/lib/index.ts
@@ -1,5 +1,18 @@
-import { Getter } from '@get-saml-metadata/interface-adapters/delivery/api/getFromFile'
+import { makeControllerComposite } from '@get-saml-metadata/interface-adapters/api/factories/makeControllerComposite'
+import { GetSamlMetadataFacade } from '@get-saml-metadata/interface-adapters/api/GetSamlMetadataFacade'
+import { IGetter } from '@get-saml-metadata/interface-adapters/delivery/protocols/IGetter'
+import { UrlOrPath } from '@get-saml-metadata/use-cases/GetExternalDataRequestModel'
+import { EventEmitter } from 'stream'
 
-const getter = new Getter()
+const makeGetter = (): IGetter => {
+  const eventBus = new EventEmitter()
+  const controller = makeControllerComposite(eventBus)
+  return new GetSamlMetadataFacade(
+    eventBus, controller
+  )
+}
 
-export const getFromFile = getter.getFromFile
+export const getFromFile = (path: UrlOrPath): any => {
+  const getter = makeGetter()
+  return getter.getFromFile(path)
+}
