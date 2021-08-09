757e6f2bd28a3668dbc3aab5d608cf3655887bcd
Christian Eland <eland.christian@gmail.com>
2021-08-09 11:23:45 -0300

    test(ValueObject): 'add unit test for `equal`'
    ref #34

---
+++ src/get-saml-metadata/src/entities/types/ValueObject.spec.ts
@@ -0,0 +1,49 @@
+/**
+ * Importing * as ... needed to adapt to jest.SpyOn requirements
+ */
+import * as helper from '@get-saml-metadata/entities/helpers/deeplyEqual'
+import { ValueObject } from '@get-saml-metadata/entities/types/ValueObject'
+jest.mock('@get-saml-metadata/entities/helpers/deeplyEqual')
+
+interface ISutProps {
+  key1: string
+  key2: string
+}
+
+const sutProps: ISutProps = {
+  key1: 'valid value 1',
+  key2: 'valid value 2'
+}
+
+const makeSut = (): ValueObject<ISutProps> => {
+  class ValidValueObject extends ValueObject<ISutProps> {}
+  return new ValidValueObject(sutProps)
+}
+
+describe('ValueObject', () => {
+  describe('equal', () => {
+    it('should call deeplyEqual once with correct values', () => {
+      const deeplyEqualSpy = jest.spyOn(helper, 'deeplyEqual')
+      const sut = makeSut()
+      const equalSut = makeSut()
+      // jest.spyOn(sut, 'equal' as any).mockReturnValueOnce('')
+      sut.equal(equalSut)
+      expect(deeplyEqualSpy).toHaveBeenCalledTimes(1)
+
+      // first param of first call
+      expect(deeplyEqualSpy.mock.calls[0][0]).toStrictEqual(equalSut.props)
+    })
+    it('should return false if deeplyEqual returns false', () => {
+      jest.spyOn(helper, 'deeplyEqual').mockReturnValueOnce(false)
+      const sut = makeSut()
+      const anotherSut = makeSut()
+      expect(sut.equal(anotherSut)).toBeFalsy()
+    })
+    it('should return true if deeplyEqual returns true', () => {
+      jest.spyOn(helper, 'deeplyEqual').mockReturnValueOnce(true)
+      const sut = makeSut()
+      const anotherSut = makeSut()
+      expect(sut.equal(anotherSut)).toBeTruthy()
+    })
+  })
+})
