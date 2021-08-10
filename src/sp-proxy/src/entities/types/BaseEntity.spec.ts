71dc43f084ea378fe0ba30f18e9453735357b3fc
Christian Eland <eland.christian@gmail.com>
2021-08-09 22:43:22 -0300

    test(BaseEntity): ensure equals return true if same id

--- src/sp-proxy/src/entities/types/BaseEntity.spec.ts
+++ src/sp-proxy/src/entities/types/BaseEntity.spec.ts
@@ -1,4 +1,6 @@
+import * as helper from '@sp-proxy/entities/helpers/isEntity'
 import { BaseEntity } from '@sp-proxy/entities/types/BaseEntity'
+jest.mock('@sp-proxy/entities/helpers/isEntity')
 
 interface IAnyProps {
   validProp: string
@@ -14,6 +16,17 @@ describe('BaseEntity', () => {
     it('should return true if is the same entity', () => {
       const sut = new AnyEntity({ validProp: 'valid value' })
       expect(sut.equals(sut)).toBeTruthy()
+    })
+    it('should return false if isEntity returns false', () => {
+      const sut = new AnyEntity({ validProp: 'valid value' })
+      jest.spyOn(helper, 'isEntity').mockReturnValueOnce(false)
+      expect(sut.equals({ thats: 'not an entity' } as any)).toBeFalsy()
+    })
+    it('should return true if same entity id', () => {
+      const sut = new AnyEntity({ validProp: 'validValue' }, 'valid id')
+      jest.spyOn(helper, 'isEntity').mockReturnValueOnce(true)
+      const another = new AnyEntity({ validProp: 'validValue' }, 'valid id')
+      expect(sut.equals(another)).toBeTruthy()
     })
   })
 })
