faca9bd585520f584c6a569bdf80de733d47bace
Christian Eland <eland.christian@gmail.com>
2021-08-09 22:17:00 -0300

    refactor: move `isEntity` to helper funcion

--- src/sp-proxy/src/entities/types/BaseEntity.ts
+++ src/sp-proxy/src/entities/types/BaseEntity.ts
@@ -1,4 +1,5 @@
 import { randomUUID } from 'crypto'
+import { isEntity } from '@sp-proxy/entities/helpers/isEntity'
 
 export abstract class BaseEntity<T> {
   protected readonly _id: string
@@ -12,9 +13,9 @@ export abstract class BaseEntity<T> {
   // Entities are compared based on their referential
   // equality.
   public equals(entity: BaseEntity<T>): boolean {
-    const isEntity = (v: any): v is BaseEntity<any> => {
-      return v instanceof BaseEntity
-    }
+    // const isEntity = (v: any): v is BaseEntity<any> => {
+    //   return v instanceof BaseEntity
+    // }
     if (entity === null || entity === undefined) {
       return false
     }
