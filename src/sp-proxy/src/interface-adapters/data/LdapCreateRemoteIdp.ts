5b535710c740b56c770868b9c71c07d233a65a70
Christian Eland <eland.christian@gmail.com>
2021-08-11 00:19:02 -0300

    fix: ensure error message is string

--- src/sp-proxy/src/interface-adapters/data/LdapCreateRemoteIdp.ts
+++ src/sp-proxy/src/interface-adapters/data/LdapCreateRemoteIdp.ts
@@ -4,17 +4,29 @@ import { ICreateRemoteIdpGateway } from '@sp-proxy/use-cases/ports/ICreateRemote
 import * as ldap from 'ldapjs'
 
 export class LdapCreateRemoteIdp implements ICreateRemoteIdpGateway {
+  /**
+   * @param client
+   * @param dn - the complete dn (i.e.: ou=remoteIdps,ou=inbound-saml,o=gluu)
+   * @param rdnAttribute - the unique identifier attribute
+   */
   constructor(
     private readonly client: ldap.Client,
     private readonly dn: string,
     private readonly rdnAttribute: string
   ) {}
 
+  /**
+   * Create entity in persistence
+   * @param remoteIdp - entity instance
+   * @returns true if sucessfully created
+   */
   async create(remoteIdp: RemoteIdp): Promise<boolean> {
     const rdn = `${this.rdnAttribute}=${remoteIdp.id}`
     const name = `${rdn},${this.dn}`
     this.client.add(name, remoteIdp.props, (err) => {
-      throw new PersistenceError(`${err.name}: ${err.message}`)
+      throw new PersistenceError(
+        `${err.name.toString()}: ${err.message.toString()}`
+      )
     })
     return true
   }
