f7d9227d853ac0511dc4bbbc80c3e771d724874e
Christian Eland <eland.christian@gmail.com>
2021-08-11 00:05:00 -0300

    chore(jest): add `.mock.ts` to ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/', 'env.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'env.ts', '.mock.ts'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'babel',
