0c72e6c1b3e6695351d1ea4625c7ec0d547c0944
Christian Eland <eland.christian@gmail.com>
2021-08-13 00:52:37 -0300

    feat: ensure facade calls `once` with `requestId`

--- src/sp-proxy/src/interface-adapters/api/CreateRemoteIdpFacade.spec.ts
+++ src/sp-proxy/src/interface-adapters/api/CreateRemoteIdpFacade.spec.ts
@@ -2,6 +2,9 @@ import { CreateRemoteIdpFacade } from '@sp-proxy/interface-adapters/api/CreateRe
 import { fakeCreateRemoteIdpRequest } from '@sp-proxy/interface-adapters/delivery/mocks/fakeCreateRemoteIdpRequest.mock'
 import { IController } from '@sp-proxy/interface-adapters/protocols/IController'
 import { IRequest } from '@sp-proxy/interface-adapters/protocols/IRequest'
+import * as crypto from 'crypto'
+import { EventEmitter } from 'stream'
+jest.mock('crypto')
 
 const makeController = (): IController => {
   class ControllerStub implements IController {
@@ -14,13 +17,16 @@ const makeController = (): IController => {
 interface SutTypes {
   sut: CreateRemoteIdpFacade
   controllerStub: IController
+  eventBusStub: EventEmitter
 }
 const makeSut = (): SutTypes => {
   const controllerStub = makeController()
-  const sut = new CreateRemoteIdpFacade(controllerStub)
+  const eventBusStub = new EventEmitter()
+  const sut = new CreateRemoteIdpFacade(controllerStub, eventBusStub)
   return {
     sut: sut,
-    controllerStub: controllerStub
+    controllerStub: controllerStub,
+    eventBusStub: eventBusStub
   }
 }
 
@@ -30,13 +36,25 @@ describe('CreateRemoteIdpFacade', () => {
   describe('createRemoteIdp', () => {
     it('should call controller with correct values', async () => {
       const { sut, controllerStub } = makeSut()
+      jest.spyOn(crypto, 'randomUUID').mockReturnValueOnce('valid request id')
       const handleSpy = jest.spyOn(controllerStub, 'handle')
       await sut.createRemoteIdp(fakeRequest.body)
       expect(handleSpy).toHaveBeenCalledTimes(1)
       expect(handleSpy).toHaveBeenCalledWith({
-        id: expect.any(String),
+        id: 'valid request id',
         body: fakeRequest.body
       })
+    })
+    it('should register listener', async () => {
+      const { sut, eventBusStub } = makeSut()
+      const onceSpy = jest.spyOn(eventBusStub, 'once')
+      jest.spyOn(crypto, 'randomUUID').mockReturnValueOnce('valid request id')
+      await sut.createRemoteIdp(fakeRequest.body)
+      expect(onceSpy).toHaveBeenCalledTimes(1)
+      expect(onceSpy).toHaveBeenCalledWith(
+        'valid request id',
+        expect.any(Function)
+      )
     })
   })
 })
--- src/sp-proxy/src/interface-adapters/api/CreateRemoteIdpFacade.ts
+++ src/sp-proxy/src/interface-adapters/api/CreateRemoteIdpFacade.ts
@@ -3,17 +3,24 @@ import { ICreateRemoteIdpFacade } from '@sp-proxy/interface-adapters/protocols/I
 import { ICreateRemoteIdpRequest } from '@sp-proxy/interface-adapters/protocols/ICreateRemoteIdpRequest'
 import { ICreateRemoteIdpResponse } from '@sp-proxy/interface-adapters/protocols/ICreateRemoteIdpResponse'
 import { randomUUID } from 'crypto'
+import { EventEmitter } from 'stream'
 
 export class CreateRemoteIdpFacade implements ICreateRemoteIdpFacade {
-  constructor(private readonly controller: IController) {}
+  constructor(
+    private readonly controller: IController,
+    private readonly eventBus: EventEmitter
+  ) {}
+
   async createRemoteIdp(
     props: ICreateRemoteIdpRequest
   ): Promise<ICreateRemoteIdpResponse> {
+    // TODO: move mapper to external Mapper class
     const requestId = randomUUID()
     await this.controller.handle({
       id: requestId,
       body: props
     })
+    this.eventBus.once(requestId, () => {})
     return { success: true }
   }
 }
