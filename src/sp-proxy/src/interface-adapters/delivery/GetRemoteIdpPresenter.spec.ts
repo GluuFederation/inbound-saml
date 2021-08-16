6e04f91689db90557b794c5e003be0283739aae2
Christian Eland <eland.christian@gmail.com>
2021-08-16 18:52:20 -0300

    feat(presenter): call mapper once with response model

---
+++ src/sp-proxy/src/interface-adapters/delivery/GetRemoteIdpPresenter.spec.ts
@@ -0,0 +1,88 @@
+// receives response model
+// maps response model to response dto (IResponse)
+// dispatch IResponse through event emitting
+
+import { GetRemoteIdpPresenter } from '@sp-proxy/interface-adapters/delivery/GetRemoteIdpPresenter'
+import { IDeliveryMapper } from '@sp-proxy/interface-adapters/protocols/IDeliveryMapper'
+import { IResponse } from '@sp-proxy/interface-adapters/protocols/IResponse'
+import { RemoteIdpDeliveryProps } from '@sp-proxy/interface-adapters/protocols/RemoteIdpDeliveryProps'
+import { IResponseModel } from '@sp-proxy/use-cases/io-models/IResponseModel'
+import { RemoteIdpUseCaseProps } from '@sp-proxy/use-cases/io-models/RemoteIdpUseCaseProps'
+import { EventEmitter } from 'stream'
+
+const makeDtoMapper = (): IDeliveryMapper<
+  IResponseModel<RemoteIdpUseCaseProps>,
+  IResponse<RemoteIdpDeliveryProps>
+> => {
+  class DtoMapperStub
+    implements
+      IDeliveryMapper<
+        IResponseModel<RemoteIdpUseCaseProps>,
+        IResponse<RemoteIdpDeliveryProps>
+      >
+  {
+    map(
+      responseModel: IResponseModel<RemoteIdpUseCaseProps>
+    ): IResponse<RemoteIdpDeliveryProps> {
+      return {
+        requestId: 'valid request id',
+        body: {
+          id: 'valid remote idp id',
+          name: 'valid remote idp name',
+          singleSignOnService: [
+            {
+              binding: 'valid binding',
+              location: 'valid location'
+            }
+          ],
+          signingCertificates: ['valid cert']
+        }
+      }
+    }
+  }
+  return new DtoMapperStub()
+}
+
+interface SutTypes {
+  eventBusStub: EventEmitter
+  dtoMapperStub: IDeliveryMapper<
+    IResponseModel<RemoteIdpUseCaseProps>,
+    IResponse<RemoteIdpDeliveryProps>
+  >
+  sut: GetRemoteIdpPresenter
+}
+
+const makeSut = (): SutTypes => {
+  const eventBusStub = new EventEmitter()
+  const dtoMapperStub = makeDtoMapper()
+  const sut = new GetRemoteIdpPresenter(dtoMapperStub, eventBusStub)
+  return {
+    eventBusStub,
+    dtoMapperStub,
+    sut
+  }
+}
+
+const fakeResponse: IResponseModel<RemoteIdpUseCaseProps> = {
+  requestId: 'valid requestId',
+  response: {
+    name: 'valid RemoteIdp name',
+    singleSignOnService: [
+      {
+        binding: 'valid binding',
+        location: 'valid location'
+      }
+    ],
+    signingCertificates: ['valid cert']
+  }
+}
+
+describe('GetRemoteIdpPresenter', () => {
+  it('should call mapper with response model', async () => {
+    const { sut, dtoMapperStub } = makeSut()
+    const mapSpy = jest.spyOn(dtoMapperStub, 'map')
+    await sut.present(fakeResponse)
+    expect(mapSpy).toHaveBeenCalledTimes(1)
+    expect(mapSpy).toHaveBeenCalledWith(fakeResponse)
+  })
+})
---
+++ src/sp-proxy/src/interface-adapters/delivery/GetRemoteIdpPresenter.ts
@@ -0,0 +1,25 @@
+import { IDeliveryMapper } from '@sp-proxy/interface-adapters/protocols/IDeliveryMapper'
+import { IResponse } from '@sp-proxy/interface-adapters/protocols/IResponse'
+import { RemoteIdpDeliveryProps } from '@sp-proxy/interface-adapters/protocols/RemoteIdpDeliveryProps'
+import { OutputBoundary } from '@sp-proxy/use-cases/io-channels/OutputBoundary'
+import { IResponseModel } from '@sp-proxy/use-cases/io-models/IResponseModel'
+import { RemoteIdpUseCaseProps } from '@sp-proxy/use-cases/io-models/RemoteIdpUseCaseProps'
+import { EventEmitter } from 'stream'
+
+export class GetRemoteIdpPresenter
+  implements OutputBoundary<RemoteIdpUseCaseProps>
+{
+  constructor(
+    private readonly dtoMapper: IDeliveryMapper<
+      IResponseModel<RemoteIdpUseCaseProps>,
+      IResponse<RemoteIdpDeliveryProps>
+    >,
+    private readonly eventBus: EventEmitter
+  ) {}
+
+  async present(
+    response: IResponseModel<RemoteIdpUseCaseProps>
+  ): Promise<void> {
+    this.dtoMapper.map(response)
+  }
+}
