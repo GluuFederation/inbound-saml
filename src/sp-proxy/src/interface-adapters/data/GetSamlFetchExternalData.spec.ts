6ad02cf39101a35ef7c02758ead7f5408e6ec97e
Christian Eland <eland.christian@gmail.com>
2021-08-17 19:24:00 -0300

    feat(fetcher): call `getFromUrl` with received url

---
+++ src/sp-proxy/src/interface-adapters/data/GetSamlFetchExternalData.spec.ts
@@ -0,0 +1,13 @@
+import * as getSamlMetadata from '@get-saml-metadata/lib/index'
+import { GetSamlFetchExternalData } from '@sp-proxy/interface-adapters/data/GetSamlFetchExternalData'
+jest.mock('@get-saml-metadata/lib/index')
+
+describe('GetSamlFetchExternalData', () => {
+  describe('fetch', () => {
+    it('should call getFromUrl once with correct url', async () => {
+      jest.spyOn(getSamlMetadata, 'getFromUrl')
+      const sut = new GetSamlFetchExternalData()
+      await sut.fetch('any url')
+    })
+  })
+})
