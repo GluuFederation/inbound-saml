6db1a0c0d4188a32f94b52ba7593819880ff4926
Christian Eland <eland.christian@gmail.com>
2021-08-17 19:18:08 -0300

    feat(getFromFile): add return type

--- src/get-saml-metadata/src/lib/getFromFile.ts
+++ src/get-saml-metadata/src/lib/getFromFile.ts
@@ -1,5 +1,6 @@
 import { makeGetComposite } from '@get-saml-metadata/interface-adapters/api/factories/makeGetComposite'
 import { GetSamlMetadataFacade } from '@get-saml-metadata/interface-adapters/api/GetSamlMetadataFacade'
+import { IFetchedData } from '@get-saml-metadata/interface-adapters/api/protocols/IFetchedData'
 import { IGetter } from '@get-saml-metadata/interface-adapters/delivery/protocols/IGetter'
 import { UrlOrPath } from '@get-saml-metadata/use-cases/GetExternalDataRequestModel'
 import { EventEmitter } from 'stream'
@@ -10,7 +11,7 @@ const makeGetter = (): IGetter => {
   return new GetSamlMetadataFacade(eventBus, controller)
 }
 
-export const getFromFile = async (path: UrlOrPath): Promise<any> => {
+export const getFromFile = async (path: UrlOrPath): Promise<IFetchedData> => {
   const getter = makeGetter()
   return await getter.get(path)
 }
