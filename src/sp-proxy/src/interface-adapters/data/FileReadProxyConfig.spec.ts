e0519fa2974fa754119af152bae1d83a09f83f46
Christian Eland <eland.christian@gmail.com>
2021-08-19 19:42:45 -0300

    feat(repository): call `JSON.parse` once with received string

--- src/sp-proxy/src/interface-adapters/data/FileReadProxyConfig.spec.ts
+++ src/sp-proxy/src/interface-adapters/data/FileReadProxyConfig.spec.ts
@@ -17,11 +17,12 @@ jest.mock('@sp-proxy/interface-adapters/data/config/env', () => {
 const mockedPath = '/valid/path/to/file.xml'
 
 describe('FileReadProxyConfig', () => {
-  const buffer = Buffer.from('a fake string')
+  const buffer = Buffer.from('a fake json string config')
   it('should call fs.readFileSync with correct params', async () => {
     const readFileSyncSpy = jest
       .spyOn(fs, 'readFileSync')
       .mockReturnValueOnce(buffer)
+    jest.spyOn(JSON, 'parse').mockReturnValueOnce('valid json')
     const sut = new FileReadProxyConfig()
     await sut.read()
     expect(readFileSyncSpy).toHaveBeenCalledTimes(1)
@@ -29,10 +30,20 @@ describe('FileReadProxyConfig', () => {
   })
   it('should call toString from received buffer', async () => {
     /// const toStringSpy = jest.spyOn(Buffer.prototype, 'toString')
+    jest.spyOn(JSON, 'parse').mockReturnValueOnce('valid json')
     const toStringSpy = jest.spyOn(buffer, 'toString')
     jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(buffer)
+    jest.spyOn(JSON, 'parse')
     const sut = new FileReadProxyConfig()
     await sut.read()
     expect(toStringSpy).toHaveBeenCalledTimes(1)
+  })
+  it('should call JSON Parse once with received string', async () => {
+    jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(buffer)
+    const parseSpy = jest.spyOn(JSON, 'parse').mockReturnValueOnce('valid json')
+    const sut = new FileReadProxyConfig()
+    await sut.read()
+    expect(parseSpy).toHaveBeenCalledTimes(1)
+    expect(parseSpy).toHaveBeenCalledWith('a fake json string config')
   })
 })
