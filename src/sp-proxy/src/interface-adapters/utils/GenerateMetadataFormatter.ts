d325b080b672b1a8bd24c5c32e4e5612b40b4066
Christian Eland <eland.christian@gmail.com>
2021-08-23 17:04:56 -0300

    feat(formatter): should return correct values

--- src/sp-proxy/src/interface-adapters/utils/GenerateMetadataFormatter.ts
+++ src/sp-proxy/src/interface-adapters/utils/GenerateMetadataFormatter.ts
@@ -2,12 +2,11 @@ import { IKeyCertFormatter } from '@sp-proxy/interface-adapters/protocols/IKeySe
 
 export class GenerateMetadataFormatter implements IKeyCertFormatter {
   async format(keyOrCert: string): Promise<string> {
-    const anystring = 'any string'
-    anystring.replace(/(\r\n|\n|\r)/gm, '')
-    anystring.replace('-----BEGIN CERTIFICATE-----', '')
-    anystring.replace('-----END CERTIFICATE-----', '')
-    anystring.replace('-----BEGIN ENCRYPTED PRIVATE KEY-----', '')
-    anystring.replace('-----END ENCRYPTED PRIVATE KEY-----', '')
-    return 'not implemented'
+    return keyOrCert
+      .replace(/(\r\n|\n|\r)/gm, '')
+      .replace('-----BEGIN CERTIFICATE-----', '')
+      .replace('-----END CERTIFICATE-----', '')
+      .replace('-----BEGIN ENCRYPTED PRIVATE KEY-----', '')
+      .replace('-----END ENCRYPTED PRIVATE KEY-----', '')
   }
 }
