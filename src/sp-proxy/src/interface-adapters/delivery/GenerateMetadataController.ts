cc4a55466931a743024192dcae0b9d89f926d5ba
Christian Eland <eland.christian@gmail.com>
2021-08-23 18:48:08 -0300

    feat(controller): call interactor with mapped request model

---
+++ src/sp-proxy/src/interface-adapters/delivery/GenerateMetadataController.ts
@@ -0,0 +1,21 @@
+import { IGenerateMetadataRequest } from '@sp-proxy/interface-adapters/delivery/dtos/IGenerateMetadataRequest'
+import { IController } from '@sp-proxy/interface-adapters/protocols/IController'
+import { IDeliveryMapper } from '@sp-proxy/interface-adapters/protocols/IDeliveryMapper'
+import { IRequest } from '@sp-proxy/interface-adapters/protocols/IRequest'
+import { GenerateMetadataRequestUseCaseParams } from '@sp-proxy/use-cases/io-models/GenerateMetadataRequestUseCaseParams'
+import { IRequestModel } from '@sp-proxy/use-cases/io-models/IRequestModel'
+import { InputBoundary } from '@sp-proxy/use-cases/ports/InputBoundary'
+
+export class GenerateMetadataController implements IController {
+  constructor(
+    private readonly mapper: IDeliveryMapper<
+      IRequest<IGenerateMetadataRequest>,
+      IRequestModel<GenerateMetadataRequestUseCaseParams>
+    >,
+    private readonly interactor: InputBoundary<GenerateMetadataRequestUseCaseParams>
+  ) {}
+
+  async handle(request: IRequest<IGenerateMetadataRequest>): Promise<void> {
+    await this.interactor.execute(this.mapper.map(request))
+  }
+}
