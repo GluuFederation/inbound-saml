00a29db3601c6ab5e89a6208f34c52e9ac2def96
Christian Eland <eland.christian@gmail.com>
2021-08-24 15:17:08 -0300

    chore: remove unused console logging

--- src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.ts
+++ src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.ts
@@ -8,18 +8,14 @@ const metadataRouter = Router()
 const adaptFacade = () => {
   return async (request: Request, response: Response) => {
     try {
-      console.log('entered catch')
       const facade = makeGenerateMetadataFacade()
-      console.log('created facade')
-      console.log(await facade.generateMetadata())
-      console.log('awaiting facade')
-      response.sendStatus(200)
+      const xmlMetadata = await facade.generateMetadata()
+      response.status(200).send(xmlMetadata)
     } catch (err) {
       if (err instanceof InvalidRequestError) {
         response.status(400).send(err.message)
       } else if (err instanceof Error) {
         response.sendStatus(500)
-        console.log(err)
       } else if (!(err instanceof Error)) {
         console.log(err)
         response.sendStatus(500)
