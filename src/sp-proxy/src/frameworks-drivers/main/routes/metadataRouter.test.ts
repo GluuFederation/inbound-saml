b8b5e06f08669bc714f26bd227008b398ac6e585
Christian Eland <eland.christian@gmail.com>
2021-08-24 15:02:21 -0300

    feat(router): return 200 if no error

--- src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.test.ts
+++ src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.test.ts
@@ -1,16 +1,39 @@
 import routes from '@sp-proxy/frameworks-drivers/main/routes'
 import { InvalidRequestError } from '@sp-proxy/interface-adapters/delivery/errors/InvalidRequestError'
 import { GenerateMetadataController } from '@sp-proxy/interface-adapters/delivery/GenerateMetadataController'
+import * as configRepo from '@sp-proxy/interface-adapters/data/FileReadProxyConfig'
 import express from 'express'
 import request from 'supertest'
+import { SpProxyConfigProps } from '@sp-proxy/entities/protocols/SpProxyConfigProps'
+import { SpProxyConfig } from '@sp-proxy/entities/SpProxyConfig'
+jest.mock('@sp-proxy/interface-adapters/data/FileReadProxyConfig')
 
-const app = express()
-app.use(routes)
 describe('metadataRoute', () => {
+  const app = express()
   beforeAll(async () => {
     // setup app for testing this route
-    const app = express()
     app.use(routes)
+    const mockedProps: SpProxyConfigProps = {
+      host: 'myhost.com',
+      requestedIdentifierFormat: 'my:name:identifier:requested',
+      authnContextIdentifierFormat: 'my:authn:name:identifier:format',
+      skipRequestCompression: true,
+      decryption: {
+        publicCertPath: process.cwd() + '/src/testdata/cert.pem',
+        privateKeyPath: process.cwd() + '/src/testdata/key.pem'
+      },
+      signing: {
+        publicCertPath: process.cwd() + '/src/testdata/cert.pem',
+        privateKeyPath: process.cwd() + '/src/testdata/key.pem'
+      }
+    }
+    const mockedConfig: SpProxyConfig = new SpProxyConfig(mockedProps)
+    jest
+      .spyOn(configRepo.FileReadProxyConfig.prototype, 'read')
+      .mockResolvedValueOnce(mockedConfig)
+
+    // cfg.database.file.proxyConfigPath =
+    //   process.cwd() + '/src/testdata/sp-proxy-config-test.json'
   })
   it('should return 400 if controller throws InvalidRequestError', async () => {
     jest
@@ -39,5 +62,8 @@ describe('metadataRoute', () => {
         throw new AnyOtherError('Any other error ocurred')
       })
     await request(app).get('/sp/metadata').expect(500)
+  })
+  it('should return status 200', async () => {
+    await request(app).get('/sp/metadata').expect(200)
   })
 })
--- src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.ts
+++ src/sp-proxy/src/frameworks-drivers/main/routes/metadataRouter.ts
@@ -8,13 +8,20 @@ const metadataRouter = Router()
 const adaptFacade = () => {
   return async (request: Request, response: Response) => {
     try {
+      console.log('entered catch')
       const facade = makeGenerateMetadataFacade()
-      await facade.generateMetadata()
+      console.log('created facade')
+      console.log(await facade.generateMetadata())
+      console.log('awaiting facade')
       response.sendStatus(200)
     } catch (err) {
       if (err instanceof InvalidRequestError) {
         response.status(400).send(err.message)
       } else if (err instanceof Error) {
+        response.sendStatus(500)
+        console.log(err)
+      } else if (!(err instanceof Error)) {
+        console.log(err)
         response.sendStatus(500)
       }
     }
