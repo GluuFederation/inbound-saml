36ce0a6c704f082e7ed70ade4cd4ed62392b6028
Christian <59786962+christian-hawk@users.noreply.github.com>
2021-08-25 14:24:45 -0300

    feat(mapper): map TrustRelation to response model params (#58)

--- src/sp-proxy/src/use-cases/io-models/GetTrByHostResponseUseCaseParams.ts
+++ src/sp-proxy/src/use-cases/io-models/GetTrByHostResponseUseCaseParams.ts
@@ -1,7 +1,8 @@
 import { RemoteIdpUseCaseParams } from '@sp-proxy/use-cases/io-models/RemoteIdpUseCaseParams'
+import { IService } from '@sp-proxy/use-cases/protocols/IService'
 
 export interface GetTrByHostResponseUseCaseParams {
   id: string
-  selectedSsoService: string
+  selectedSsoService: IService
   remoteIdp: RemoteIdpUseCaseParams
 }
