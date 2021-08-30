d873a86786df55e7c912cfb8abc340556378811f
Christian Eland <eland.christian@gmail.com>
2021-08-30 14:26:38 -0300

    test(router): persist object w/ expected data

--- src/sp-proxy/src/frameworks-drivers/main/routes/addTrFromMetadataRouter.test.ts
+++ src/sp-proxy/src/frameworks-drivers/main/routes/addTrFromMetadataRouter.test.ts
@@ -6,20 +6,29 @@ import { AddTrFromMetadataController } from '@sp-proxy/interface-adapters/delive
 import { InvalidRequestError } from '@sp-proxy/interface-adapters/delivery/errors/InvalidRequestError'
 import { mockValidXmlDataEndpoint } from '@sp-proxy/frameworks-drivers/main/mocks/externalMetadataUrl.mock'
 import nock from 'nock'
-// import app from '@sp-proxy/frameworks-drivers/main/server'
-
+import { Collection, MongoClient } from 'mongodb'
+import config from '@sp-proxy/interface-adapters/config/env'
 jest.mock('@sp-proxy/interface-adapters/data/FileReadProxyConfig')
 
 const app = express()
 
 app.use(express.json())
 describe('addTrFromMetadataRouter', () => {
+  let mongoClient: MongoClient
+  let connection: MongoClient
+  let collection: Collection
   beforeAll(async () => {
-    // setup app for testing this route
+    // setup app and db for testing this route
+    mongoClient = new MongoClient(config.database.mongo.uri)
+    connection = await mongoClient.connect()
+    collection = connection
+      .db(config.database.mongo.dbName)
+      .collection(config.database.mongo.collections.trustRelations)
     app.use(routes)
     mockSpProxyConfig()
   })
   afterAll(async () => {
+    await connection.close()
     jest.clearAllMocks()
     nock.cleanAll()
   })
@@ -91,5 +100,24 @@ describe('addTrFromMetadataRouter', () => {
         url: 'https://remoteIdp.com/metadata'
       })
       .expect({ creation: 'success' })
+  })
+  it('should persist object with expected data', async () => {
+    await collection.drop()
+    mockValidXmlDataEndpoint()
+    await request(app)
+      .post(endpoint)
+      .send({
+        name: 'valid name integration',
+        url: 'https://remoteIdp.com/metadata'
+      })
+      .expect({ creation: 'success' })
+    expect(await collection.countDocuments()).toEqual(1)
+    const found = await collection.findOne()
+    expect(found).not.toBeNull()
+    if (found != null) {
+      expect(found.trustRelation.props.remoteIdp.props.name).toEqual(
+        'valid name integration'
+      )
+    }
   })
 })
