ed9bab47e7359af0a379f97dd5e4d81e5009778b
Christian Eland <eland.christian@gmail.com>
2021-08-30 16:37:28 -0300

    refactor(middleware): reduce complexity

--- src/sp-proxy/src/frameworks-drivers/main/middleware/adminBasicPostAuth.ts
+++ src/sp-proxy/src/frameworks-drivers/main/middleware/adminBasicPostAuth.ts
@@ -8,21 +8,21 @@ import { NextFunction, Request, Response } from 'express'
 export const adminBasicPostAuth = () => {
   return (request: Request, response: Response, next: NextFunction) => {
     const encoded = request.headers.authorization?.replace('Basic ', '')
-    if (encoded != null) {
+    if (encoded == null) {
+      response.sendStatus(401)
+    } else {
       const string = Buffer.from(encoded?.toString(), 'base64').toString()
       const credentials = string.split(':')
       const username = credentials[0]
       const password = credentials[1]
-      if (username !== serverConfig.adminUser) {
-        response.sendStatus(401)
-        response.end()
-      } else if (password !== serverConfig.adminPassword) {
-        response.sendStatus(401)
-      } else {
+      if (
+        username === serverConfig.adminUser &&
+        password === serverConfig.adminPassword
+      ) {
         next()
+      } else {
+        response.sendStatus(401)
       }
-    } else {
-      response.sendStatus(401)
     }
   }
 }
