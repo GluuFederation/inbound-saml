eb3a66ef32dc8f05ff87074a292079043a958fb0
Christian Eland <eland.christian@gmail.com>
2021-09-03 22:51:07 -0300

    feat(transformer): call loader with signing cert path

---
+++ packages/sp-proxy/src/use-cases/transformers/ReadSpProxyConfigUseCaseTransformer.spec.ts
@@ -0,0 +1,66 @@
+// receive entity
+// load certificate
+// apply cert formatting
+// return rensponse model
+
+import { SpProxyConfigProps } from '@sp-proxy/entities/protocols/SpProxyConfigProps'
+import { SpProxyConfig } from '@sp-proxy/entities/SpProxyConfig'
+import { IKeyCertLoader } from '@sp-proxy/use-cases/protocols/IKeyCertLoader'
+import { IKeyCertFormatter } from '@sp-proxy/use-cases/protocols/IKeySetFormatter'
+import { ReadSpProxyConfigUseCaseTransformer } from '@sp-proxy/use-cases/transformers/ReadSpProxyConfigUseCaseTransformer'
+
+const makeLoader = (): IKeyCertLoader => {
+  class LoaderStub implements IKeyCertLoader {
+    async load(path: string): Promise<string> {
+      return 'stubbed cert string'
+    }
+  }
+  return new LoaderStub()
+}
+
+const makeFormatter = (): IKeyCertFormatter => {
+  class FormatterStub implements IKeyCertFormatter {
+    async format(keyOrCert: string): Promise<string> {
+      return 'stubbed formatted key or cert'
+    }
+  }
+  return new FormatterStub()
+}
+
+interface SutTypes {
+  sut: ReadSpProxyConfigUseCaseTransformer
+  loaderStub: IKeyCertLoader
+  formatterStub: IKeyCertFormatter
+}
+
+const makeSut = (): SutTypes => {
+  const loaderStub = makeLoader()
+  const formatterStub = makeFormatter()
+  const sut = new ReadSpProxyConfigUseCaseTransformer(loaderStub, formatterStub)
+  return { sut, loaderStub, formatterStub }
+}
+
+const spProxyConfigProps: SpProxyConfigProps = {
+  host: 'fake stubbed host',
+  requestedIdentifierFormat: 'fake stubbed identifier format',
+  authnContextIdentifierFormat: 'fake stubbed authn context identifier format',
+  skipRequestCompression: true,
+  decryption: {
+    privateKeyPath: 'fake /valid/pvk/path.pem',
+    publicCertPath: 'fake /valid/cert/path.pem'
+  }
+}
+
+const fakeSpProxyConfig = new SpProxyConfig(spProxyConfigProps)
+
+describe('ReadSpProxyConfigUseCaseTransformer', () => {
+  it('should call loader with signing cert path', async () => {
+    const { sut, loaderStub } = makeSut()
+    const loadSpy = jest.spyOn(loaderStub, 'load')
+    await sut.transform(fakeSpProxyConfig)
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(
+      fakeSpProxyConfig.props.decryption.publicCertPath
+    )
+  })
+})
---
+++ packages/sp-proxy/src/use-cases/transformers/ReadSpProxyConfigUseCaseTransformer.ts
@@ -0,0 +1,44 @@
+import { SpProxyConfig } from '@sp-proxy/entities/SpProxyConfig'
+import { IKeyCertLoader } from '@sp-proxy/use-cases/protocols/IKeyCertLoader'
+import { IResponseModel } from '@sp-proxy/use-cases/io-models/IResponseModel'
+import { ReadSpProxyConfigResponseUseCaseParams } from '@sp-proxy/use-cases/io-models/ReadSpProxyConfigResponseUseCaseParams'
+import { ITransformer } from '@sp-proxy/use-cases/ports/ITransformer'
+import { IKeyCertFormatter } from '@sp-proxy/use-cases/protocols/IKeySetFormatter'
+
+export class ReadSpProxyConfigUseCaseTransformer
+  implements
+    ITransformer<
+      SpProxyConfig,
+      IResponseModel<ReadSpProxyConfigResponseUseCaseParams>
+    >
+{
+  /**
+   * Creates an instance of GenerateMetadataTransformer.
+   * @param {IKeyCertLoader} loader
+   * @param {IKeyCertFormatter} formatter
+   * @memberof GenerateMetadataTransformer
+   */
+  constructor(
+    private readonly loader: IKeyCertLoader,
+    private readonly formatter: IKeyCertFormatter
+  ) {}
+
+  async transform(
+    spProxyConfig: SpProxyConfig
+  ): Promise<IResponseModel<ReadSpProxyConfigResponseUseCaseParams>> {
+    await this.loader.load(spProxyConfig.props.decryption.publicCertPath)
+    return {
+      requestId: '',
+      response: {
+        host: 'string',
+        requestedIdentifierFormat: 'string',
+        authnContextIdentifierFormat: 'string',
+        skipRequestCompression: true,
+        decryption: {
+          privateKey: 'string',
+          cert: 'string'
+        }
+      }
+    }
+  }
+}
