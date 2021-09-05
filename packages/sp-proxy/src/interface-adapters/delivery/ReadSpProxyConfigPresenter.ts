0a9f95f209de7032eda92eecfae0da312a31f018
Christian <59786962+christian-hawk@users.noreply.github.com>
2021-09-05 18:36:33 -0300

    feat(readSpProxyConfig): add result dispatcher (#85)
    * feat(presenter): call mapper with received response model
    
    * test(presenter): throw if mapper throws
    
    * feat(presenter): call eventBus emit with mapped IResponse DTO
    
    * feat(mapper): map response model to dto w/ signing props

---
+++ packages/sp-proxy/src/interface-adapters/delivery/ReadSpProxyConfigPresenter.ts
@@ -0,0 +1,27 @@
+import { IReadSpProxyConfigResponse } from '@sp-proxy/interface-adapters/delivery/dtos/IReadSpProxyConfigResponse'
+import { IDeliveryMapper } from '@sp-proxy/interface-adapters/protocols/IDeliveryMapper'
+import { IResponse } from '@sp-proxy/interface-adapters/protocols/IResponse'
+import { IResponseModel } from '@sp-proxy/use-cases/io-models/IResponseModel'
+import { ReadSpProxyConfigResponseUseCaseParams } from '@sp-proxy/use-cases/io-models/ReadSpProxyConfigResponseUseCaseParams'
+import { OutputBoundary } from '@sp-proxy/use-cases/ports/OutputBoundary'
+import { EventEmitter } from 'stream'
+
+export class ReadSpProxyConfigPresenter
+  implements
+    OutputBoundary<IResponseModel<ReadSpProxyConfigResponseUseCaseParams>>
+{
+  constructor(
+    private readonly mapper: IDeliveryMapper<
+      IResponseModel<ReadSpProxyConfigResponseUseCaseParams>,
+      IResponse<IReadSpProxyConfigResponse>
+    >,
+    private readonly eventBus: EventEmitter
+  ) {}
+
+  async present(
+    responseModel: IResponseModel<ReadSpProxyConfigResponseUseCaseParams>
+  ): Promise<void> {
+    const responseDto = this.mapper.map(responseModel)
+    this.eventBus.emit(responseDto.requestId, responseDto)
+  }
+}
---
+++ packages/sp-proxy/src/interface-adapters/delivery/dtos/IReadSpProxyConfigResponse.ts
@@ -0,0 +1,14 @@
+export interface IReadSpProxyConfigResponse {
+  host: string
+  requestedIdentifierFormat: string
+  authnContextIdentifierFormat: string
+  skipRequestCompression: boolean
+  decryption: {
+    privateKey: string
+    cert: string
+  }
+  signing?: {
+    privateKey: string
+    cert: string
+  }
+}
