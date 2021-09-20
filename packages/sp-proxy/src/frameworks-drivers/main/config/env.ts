aeeda150ae6d569fc0b044e9453d8056439732fb
christian-hawk <eland.christian@gmail.com>
2021-09-20 11:03:10 +0000

    feat(server): make server options configurable

--- packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
@@ -1,5 +1,12 @@
 export default {
   adminUser: process.env.ADMIN_USER ?? 'admin',
   adminPassword: process.env.ADMIN_PWD ?? 'admin',
-  logLevel: process.env.LOG_LEVEL ?? 'debug'
+  logLevel: process.env.LOG_LEVEL ?? 'debug',
+  port: process.env.PORT ?? '443',
+  tlsCertPath:
+    process.env.TLS_CERT_PATH ??
+    `${process.cwd()}/packages/sp-proxy/src/frameworks-drivers/main/cert/cert.pem`,
+  tlsKeyPath:
+    process.env.TLS_KEY_PATH ??
+    `${process.cwd()}/packages/sp-proxy/src/frameworks-drivers/main/cert/key.pem`
 }
