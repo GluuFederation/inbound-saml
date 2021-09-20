85f4be6db1c728bb89dd95356f201e6484083aa0
christian-hawk <eland.christian@gmail.com>
2021-09-20 11:13:10 +0000

    fix(server): change default port to 5000

--- packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
@@ -2,7 +2,7 @@ export default {
   adminUser: process.env.ADMIN_USER ?? 'admin',
   adminPassword: process.env.ADMIN_PWD ?? 'admin',
   logLevel: process.env.LOG_LEVEL ?? 'debug',
-  port: process.env.PORT ?? '443',
+  port: process.env.PORT ?? '5000',
   tlsCertPath:
     process.env.TLS_CERT_PATH ??
     `${process.cwd()}/packages/sp-proxy/src/frameworks-drivers/main/cert/cert.pem`,
