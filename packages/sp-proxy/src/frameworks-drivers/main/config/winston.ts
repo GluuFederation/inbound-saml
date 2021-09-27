29f240c60badd5df0a3d7fe5766f563420bf9e48
christian-hawk <eland.christian@gmail.com>
2021-09-27 15:20:42 +0000

    feat(logger): add colorize to console logging

--- packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
@@ -4,5 +4,5 @@ import cfg from '@sp-proxy/frameworks-drivers/main/config/env'
 export const defaultLogger = createLogger({
   level: cfg.logLevel,
   transports: [new transports.Console()],
-  format: format.simple()
+  format: format.combine(format.colorize(), format.simple())
 })
