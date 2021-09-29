56a4bc1be23cf2bd8e82803b0aae074753ea7d72
christian-hawk <eland.christian@gmail.com>
2021-09-29 20:14:55 +0000

    fix: remove unused function

--- packages/sp-proxy/src/frameworks-drivers/main/routes/authenticateCallbackRouter.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/routes/authenticateCallbackRouter.ts
@@ -3,9 +3,7 @@ import { NextFunction, Request, Response, Router, urlencoded } from 'express'
 import session from 'express-session'
 import { makeReadSpProxyConfigFacade } from '../factories/makeReadSpProxyConfigFacade'
 import { generatePostProfileForm } from '../helpers/generatePostProfileForm'
-import { WinstonLogger } from '../logger/WinstonLogger'
 
-const logger = WinstonLogger.getInstance()
 const authenticateCallbackRouter = Router()
 const sessionOptions: session.SessionOptions = {
   secret: 'any secret',
@@ -17,21 +15,6 @@ authenticateCallbackRouter.use(session(sessionOptions))
 
 authenticateCallbackRouter.use(passport.initialize())
 authenticateCallbackRouter.use(passport.session())
-authenticateCallbackRouter.post(
-  '/callback',
-  (req: Request, res: Response, next: NextFunction) => {
-    logger.debug('/callback called')
-    next()
-  },
-  passport.authenticate('saml', { failureFlash: true }),
-  (req: Request, res: Response) => {
-    if (req.isAuthenticated()) {
-      res.end('AUTHENTICATED')
-    } else {
-      res.end('FAILURE!')
-    }
-  }
-)
 
 authenticateCallbackRouter.get('/isauth', (req: Request, res: Response) => {
   res.send(req.isAuthenticated())
