24190e6e75ac28b7ae298cdfb7afb3e727713b85
christian-hawk <eland.christian@gmail.com>
2021-10-01 14:52:30 +0000

    refactor(authenticate): remove unneeded throwing

--- packages/sp-proxy/src/frameworks-drivers/main/routes/authenticateRouter.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/routes/authenticateRouter.ts
@@ -1,5 +1,5 @@
 import passport from '@sp-proxy/frameworks-drivers/main/config/passportMiddleware'
-import { NextFunction, Request, Response, Router } from 'express'
+import { Router } from 'express'
 import session from 'express-session'
 
 const authenticateRouter = Router()
@@ -11,17 +11,10 @@ const sessionOptions: session.SessionOptions = {
 
 authenticateRouter.use(session(sessionOptions))
 
-// authenticateRouter.use(urlencoded({ extended: false }))
 authenticateRouter.use(passport.initialize())
 authenticateRouter.use(passport.session())
 authenticateRouter.get(
   '/authenticate',
-  (req: Request, res: Response, next: NextFunction) => {
-    if (req.query.providerHost == null) {
-      throw Error('no provider host')
-    }
-    next()
-  },
   passport.authenticate('saml', { failureRedirect: '/', failureFlash: true })
 )
 
