cd7e536532aa7ab855d986c7d5922037fa42cd1a
christian <eland.christian@gmail.com>
2021-11-03 19:44:16 -0300

    feat(dataMapper): call GetTRMapper accordingly

---
+++ packages/sp-proxy/src/interface-adapters/data/mappers/ListTRsMongoMapper.ts
@@ -0,0 +1,18 @@
+import { TrustRelation } from '@sp-proxy/entities/TrustRelation'
+import { IDataMapper } from '@sp-proxy/interface-adapters/protocols/IDataMapper'
+import { Document as MongoDocument } from 'mongodb'
+import { GetTRMongoMapper } from './GetTRMongoMapper'
+
+export class ListTRsMongoMapper
+  implements IDataMapper<MongoDocument[], TrustRelation[]>
+{
+  async map(mappedFrom: MongoDocument[]): Promise<TrustRelation[]> {
+    const getTRMongoMapper = new GetTRMongoMapper()
+    const trustRelations: TrustRelation[] = []
+    for (const document of mappedFrom) {
+      trustRelations.push(await getTRMongoMapper.map(document as any))
+      /// console.log(trustRelations)
+    }
+    return '' as any
+  }
+}
