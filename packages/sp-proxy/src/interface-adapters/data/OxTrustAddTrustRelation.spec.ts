03379af891b4fd8e0c39110c4637c585268fab75
christian <eland.christian@gmail.com>
2022-01-25 09:59:32 -0300

    feat(gateway): call post with api uri and path

--- packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.spec.ts
+++ packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.spec.ts
@@ -4,15 +4,22 @@ import { IAddTrGateway } from '@sp-proxy/use-cases/ports/IAddTrGateway'
 import axios from 'axios'
 import { OxTrustAddTrustRelation } from './OxTrustAddTrustRelation'
 
+const oxTrustApiSettings = {
+  uri: 'valid uri',
+  path: 'valid path',
+  trustRelationEndpoint: 'valid endpoint'
+}
+
 const makeSut = (): IAddTrGateway => {
-  return new OxTrustAddTrustRelation()
+  return new OxTrustAddTrustRelation(oxTrustApiSettings)
 }
 
 describe('OxTrustAddTrustRelation', () => {
-  it('should call axios post once', async () => {
+  it('should call post with api uri and path', async () => {
     const postSpy = jest.spyOn(axios, 'post').mockResolvedValue('success')
     const sut = makeSut()
     await sut.add('valid trust relation' as any)
     expect(postSpy).toHaveBeenCalledTimes(1)
+    expect(postSpy).toHaveBeenCalledWith('valid uri/valid path/valid endpoint')
   })
 })
--- packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.ts
+++ packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.ts
@@ -2,9 +2,20 @@ import { TrustRelation } from '@sp-proxy/entities/TrustRelation'
 import { IAddTrGateway } from '@sp-proxy/use-cases/ports/IAddTrGateway'
 import axios from 'axios'
 
+interface IOxTrustApiSettings {
+  uri: string
+  path: string
+  trustRelationEndpoint: string
+}
+
 export class OxTrustAddTrustRelation implements IAddTrGateway {
+  private readonly postUrl: string
+  constructor(private readonly oxTrustApiSettings: IOxTrustApiSettings) {
+    this.postUrl = `${oxTrustApiSettings.uri}/${oxTrustApiSettings.path}/${oxTrustApiSettings.trustRelationEndpoint}`
+  }
+
   async add(trustRelation: TrustRelation): Promise<boolean> {
-    await axios.post('any url')
+    await axios.post(this.postUrl)
     return true
   }
 }
