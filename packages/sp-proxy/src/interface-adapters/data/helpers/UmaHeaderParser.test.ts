6feea3f8f617a8c832e9b6be2f0f33ab91bda222
christian <eland.christian@gmail.com>
2022-02-21 00:11:45 -0300

    feat(UmaHeaderParser): throws if no UMA realm value

---
+++ packages/sp-proxy/src/interface-adapters/data/helpers/UmaHeaderParser.test.ts
@@ -0,0 +1,20 @@
+import { UmaHeaderError } from '../errors/UmaHeaderError'
+import { IUmaHeaderParser } from '../protocols/IUmaHeaderParser'
+import { UmaHeaderParser } from './UmaHeaderParser'
+
+const makesut = (): IUmaHeaderParser => {
+  return new UmaHeaderParser()
+}
+
+describe('UMAHeaderParser', () => {
+  describe('parse', () => {
+    it('should throw if no UMA realm header key found', () => {
+      const invalidHeader =
+        'host_id=apitest.techno24x7.com, as_uri=https://apitest.techno24x7.com/.well-known/uma2-configuration, ticket=e72ae32f-ad6d-458d-bf18-d34cd5081fb3'
+      const sut = makesut()
+      expect(() => {
+        sut.parse(invalidHeader)
+      }).toThrowError(UmaHeaderError)
+    })
+  })
+})
--- packages/sp-proxy/src/interface-adapters/data/helpers/UmaHeaderParser.ts
+++ packages/sp-proxy/src/interface-adapters/data/helpers/UmaHeaderParser.ts
@@ -1,9 +1,17 @@
+import { UmaHeaderError } from '../errors/UmaHeaderError'
 import { IUmaHeaderParser } from '../protocols/IUmaHeaderParser'
 import { IWwwAuthenticate } from '../protocols/IWwwAuthenticate'
 
 export class UmaHeaderParser implements IUmaHeaderParser {
   parse(wwwAuthenticateValue: string): IWwwAuthenticate {
-    wwwAuthenticateValue.split(', ')
+    const splittedValues = wwwAuthenticateValue.split(', ')
+    const umaRealm = splittedValues.find((item) => item.includes('UMA'))
+    if (umaRealm == null) {
+      throw new UmaHeaderError('Missing values in www-authenticate header')
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const umaRealmValue = umaRealm.split('=')[1]
+    }
     return '' as any
   }
 }
