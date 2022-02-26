388b7f6329d09f655172156fb4294d3bc547f842
christian <eland.christian@gmail.com>
2022-02-26 15:18:28 -0300

    feat(addTrustRelation): call data model mapper with entity

--- packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.spec.ts
+++ packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.spec.ts
@@ -1,15 +1,17 @@
 // should call persistence api once
 
+import { TrustRelation } from '@sp-proxy/entities/TrustRelation'
 import { IAddTrGateway } from '@sp-proxy/use-cases/ports/IAddTrGateway'
 import axios from 'axios'
+import { IDataMapper } from '../protocols/IDataMapper'
+import { TrustRelationDataModel } from './models/TrustRelationDataModel'
 import { OxTrustAddTrustRelation } from './OxTrustAddTrustRelation'
 import { IOxTrustApiSettings } from './protocols/IOxTrustApiSettings'
 
-// const oxTrustApiSettings = {
-//   uri: 'valid uri',
-//   path: 'valid path',
-//   trustRelationEndpoint: 'valid endpoint'
-// }
+// receives trust relation entity
+// maps entity to data model
+// calls axios post with data model
+// validate response (throw error if error)
 
 const makeSettings = (): IOxTrustApiSettings => {
   const settings: IOxTrustApiSettings = {
@@ -23,21 +25,48 @@ const makeSettings = (): IOxTrustApiSettings => {
   return settings
 }
 
+const makeDataModelMapper = (): IDataMapper<
+  TrustRelation,
+  TrustRelationDataModel
+> => {
+  class DataModelMapperStub
+    implements IDataMapper<TrustRelation, TrustRelationDataModel>
+  {
+    async map(mappedFrom: TrustRelation): Promise<TrustRelationDataModel> {
+      return 'valid TrustRelationDataModel' as any
+    }
+  }
+  return new DataModelMapperStub()
+}
+
 interface SutTypes {
   oxTrustApiSettings: IOxTrustApiSettings
+  dataModelMapperStub: IDataMapper<TrustRelation, TrustRelationDataModel>
   sut: IAddTrGateway
 }
 
 const makeSut = (): SutTypes => {
   const oxTrustApiSettings = makeSettings()
-  const sut = new OxTrustAddTrustRelation(oxTrustApiSettings)
+  const dataModelMapperStub = makeDataModelMapper()
+  const sut = new OxTrustAddTrustRelation(
+    oxTrustApiSettings,
+    dataModelMapperStub
+  )
   return {
     oxTrustApiSettings,
+    dataModelMapperStub,
     sut
   }
 }
 
 describe('OxTrustAddTrustRelation', () => {
+  it('should call data model mapper with entity', async () => {
+    jest.spyOn(axios, 'post').mockResolvedValue('success')
+    const { dataModelMapperStub, sut } = makeSut()
+    const mapSpy = jest.spyOn(dataModelMapperStub, 'map')
+    await sut.add('valid trust relation entity' as any)
+    expect(mapSpy).toHaveBeenCalledWith('valid trust relation entity')
+  })
   it('should call post with api uri and path', async () => {
     const postSpy = jest.spyOn(axios, 'post').mockResolvedValue('success')
     const { sut, oxTrustApiSettings } = makeSut()
--- packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.ts
+++ packages/sp-proxy/src/interface-adapters/data/OxTrustAddTrustRelation.ts
@@ -1,16 +1,26 @@
 import { TrustRelation } from '@sp-proxy/entities/TrustRelation'
 import { IAddTrGateway } from '@sp-proxy/use-cases/ports/IAddTrGateway'
 import axios from 'axios'
+import { IDataMapper } from '../protocols/IDataMapper'
+import { TrustRelationDataModel } from './models/TrustRelationDataModel'
 import { IOxTrustApiSettings } from './protocols/IOxTrustApiSettings'
 
 export class OxTrustAddTrustRelation implements IAddTrGateway {
   private readonly postUrl: string
-  constructor(private readonly oxTrustApiSettings: IOxTrustApiSettings) {
+
+  constructor(
+    private readonly oxTrustApiSettings: IOxTrustApiSettings,
+    private readonly addTrustRelationOxTrustMapper: IDataMapper<
+      TrustRelation,
+      TrustRelationDataModel
+    >
+  ) {
     const trustRelationEndpoint = 'trusted-idp'
     this.postUrl = `https://${oxTrustApiSettings.host}/${oxTrustApiSettings.completePath}/${trustRelationEndpoint}`
   }
 
   async add(trustRelation: TrustRelation): Promise<boolean> {
+    await this.addTrustRelationOxTrustMapper.map(trustRelation)
     await axios.post(this.postUrl)
     return true
   }
