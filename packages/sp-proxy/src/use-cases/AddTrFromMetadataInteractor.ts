ec947a31b48a67575ccebdce571ab0658b3b261d
christian <eland.christian@gmail.com>
2022-02-26 15:49:34 -0300

    fix(interactor): remove remoteIdp repository call
    as remote idp entity is nested in TrustRelation dataModel, it's not needed here anymore.

--- packages/sp-proxy/src/use-cases/AddTrFromMetadataInteractor.ts
+++ packages/sp-proxy/src/use-cases/AddTrFromMetadataInteractor.ts
@@ -60,8 +60,6 @@ export class AddTrFromMetadataInteractor
       host: request.request.host
     })
 
-    await this.createRemoteIdpGateway.create(remoteIdp)
-
     const trustRelation = await this.trWithDefaultFactory.make({
       remoteIdp: remoteIdp
     })
