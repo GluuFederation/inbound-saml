4257ef5703671ca5fc9dd327b76e34496a9f6a98
christian <eland.christian@gmail.com>
2022-07-04 13:22:27 -0300

    test(validator): throws if param is null

--- packages/sp-proxy/src/interface-adapters/data/validators/NotNullOrUndefinedValidator.test.ts
+++ packages/sp-proxy/src/interface-adapters/data/validators/NotNullOrUndefinedValidator.test.ts
@@ -1,11 +1,16 @@
 import { NotNullOrUndefinedValidator } from './NotNullOrUndefinedValidator'
 
 describe('NotNullOrUndefinedValidator', () => {
-  it('should throw if param is null', () => {
+  it('should throw if param is undefined', () => {
     const sut = new NotNullOrUndefinedValidator()
     expect(() => sut.isValid(undefined)).toThrow(
       'InvalidData: Value cannot be null or undefined'
     )
   })
-  it('should throw if param is undefined', () => {})
+  it('should throw if param is null', () => {
+    const sut = new NotNullOrUndefinedValidator()
+    expect(() => sut.isValid(null)).toThrow(
+      'InvalidData: Value cannot be null or undefined'
+    )
+  })
 })
