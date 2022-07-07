dff31ee4673a5fe4865c22dea21e64eb1f6947eb
christian <eland.christian@gmail.com>
2022-07-07 16:04:49 -0300

    fix(validator): accept undefined

--- packages/sp-proxy/src/interface-adapters/data/validators/NotNullOrUndefinedValidator.ts
+++ packages/sp-proxy/src/interface-adapters/data/validators/NotNullOrUndefinedValidator.ts
@@ -1,7 +1,7 @@
 import { IValidator } from '../protocols/IValidator'
 
 export class NotNullOrUndefinedValidator implements IValidator {
-  isValid(value: any): boolean {
+  isValid(value: any | undefined): boolean {
     if (value == null) {
       throw new Error('InvalidData: Value cannot be null or undefined')
     }
