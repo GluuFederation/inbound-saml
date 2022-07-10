acc98fb513f3d8e9f7f2a7b44125146de6f413ea
christian <eland.christian@gmail.com>
2022-07-10 20:00:12 -0300

    feat(serverConfig): parse int and bool from env

--- packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/env.ts
@@ -1,9 +1,17 @@
+import { StringParser } from '../helpers/StringParser'
+import { BooleanStringType } from '../protocols/BooleanStringType'
+
+const stringParser = new StringParser()
+
+const portString = process.env.INBOUND_SAML_PORT ?? '5000'
+const useTlsString = process.env.INBOUND_SAML_USE_TLS ?? 'false'
+
 export default {
   adminUser: process.env.INBOUND_SAML_ADMIN_USER ?? 'admin',
   adminPassword: process.env.INBOUND_SAML_ADMIN_PWD ?? 'admin',
   logLevel: process.env.INBOUND_SAML_LOG_LEVEL ?? 'debug',
-  port: process.env.INBOUND_SAML_PORT ?? '5000',
-  useTls: process.env.INBOUND_SAML_USE_TLS ?? true,
+  port: stringParser.stringToInt(portString),
+  useTls: stringParser.stringToBool(useTlsString as BooleanStringType),
   tlsCertPath:
     process.env.INBOUND_SAML_TLS_CERT_PATH ??
     `${process.cwd()}/packages/sp-proxy/src/frameworks-drivers/main/cert/cert.pem`,
