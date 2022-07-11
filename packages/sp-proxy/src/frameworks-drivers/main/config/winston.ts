80f4591de7260e020bda90f3fb859f42a0876741
christian <eland.christian@gmail.com>
2022-07-11 12:38:20 -0300

    feat(logger): setup daily rotation

--- packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
@@ -1,14 +1,32 @@
 import cfg from '@sp-proxy/frameworks-drivers/main/config/env'
 import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+import { FileTransportInstance } from 'winston/lib/winston/transports'
 
-const fileTransport = new transports.File({
-  filename: './log/inbound_saml.log'
-})
+const makeFileTransport = (): FileTransportInstance => {
+  return new transports.File({
+    filename: './log/inbound_saml.log',
+    options: { flags: 'w' }
+  })
+}
+
+let fileTransport = makeFileTransport()
+
+const dailyRotateLogOptions: DailyRotateFile.DailyRotateFileTransportOptions = {
+  filename: './log/inbound_saml-%DATE%.log',
+  handleExceptions: true,
+  datePattern: 'YYYY-MM-DD',
+  maxSize: '20m'
+}
+
+const dailyTransport: DailyRotateFile = new DailyRotateFile(
+  dailyRotateLogOptions
+)
 
 export const defaultLogger = createLogger({
   exitOnError: false,
   level: cfg.logLevel,
-  transports: [new transports.Console(), fileTransport],
+  transports: [new transports.Console(), fileTransport, dailyTransport],
   format: format.combine(
     format.splat(),
     format.padLevels(),
@@ -21,4 +39,10 @@ export const defaultLogger = createLogger({
         `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`
     )
   )
+})
+
+dailyTransport.on('rotate', () => {
+  defaultLogger.remove(fileTransport)
+  fileTransport = makeFileTransport()
+  defaultLogger.add(fileTransport)
 })
