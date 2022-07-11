3deb548915f5e4988515e10e4e5f253d02735f77
christian <eland.christian@gmail.com>
2022-07-11 11:20:47 -0300

    feat(logging): setup according to gluu-server patterns

--- packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
+++ packages/sp-proxy/src/frameworks-drivers/main/config/winston.ts
@@ -1,8 +1,24 @@
-import { createLogger, format, transports } from 'winston'
 import cfg from '@sp-proxy/frameworks-drivers/main/config/env'
+import { createLogger, format, transports } from 'winston'
+
+const fileTransport = new transports.File({
+  filename: './log/inbound_saml.log'
+})
 
 export const defaultLogger = createLogger({
+  exitOnError: false,
   level: cfg.logLevel,
-  transports: [new transports.Console()],
-  format: format.simple()
+  transports: [new transports.Console(), fileTransport],
+  format: format.combine(
+    format.splat(),
+    format.padLevels(),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss.SSSZZ'
+    }),
+    format.printf(
+      (info) =>
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`
+    )
+  )
 })
