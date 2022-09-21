234d6012f06c34a84b40afb31b88c9b3480f9c36
christian-hawk <eland.christian@gmail.com>
2022-09-21 23:14:02 +0000

    test(KeyCertFormatter): dont use single line formatting

--- packages/sp-proxy/src/use-cases/protocols/BaseKeyCertFormatter.spec.ts
+++ packages/sp-proxy/src/use-cases/protocols/BaseKeyCertFormatter.spec.ts
@@ -1,21 +1,24 @@
 import { BaseKeyCertFormatter } from '@sp-proxy/use-cases/protocols/BaseKeyCertFormatter'
 
 describe('BaseKeyCertFormatter', () => {
-  it('should call replace with correct values', async () => {
+  it('should NOT call replace with correct values', async () => {
     const fakeString = 'any string'
     const replaceSpy = jest.spyOn(String.prototype, 'replace')
     class ConcreteKeyCertFormater extends BaseKeyCertFormatter {}
     const sut = new ConcreteKeyCertFormater()
     await sut.format(fakeString)
-    expect(replaceSpy).toHaveBeenCalledTimes(5)
-    expect(replaceSpy).toHaveBeenCalledWith(/(\r\n|\n|\r)/gm, '')
-    expect(replaceSpy).toHaveBeenCalledWith('-----BEGIN CERTIFICATE-----', '')
-    expect(replaceSpy).toHaveBeenCalledWith('-----END CERTIFICATE-----', '')
-    expect(replaceSpy).toHaveBeenCalledWith(
+    expect(replaceSpy).toHaveBeenCalledTimes(0)
+    expect(replaceSpy).not.toHaveBeenCalledWith(/(\r\n|\n|\r)/gm, '')
+    expect(replaceSpy).not.toHaveBeenCalledWith(
+      '-----BEGIN CERTIFICATE-----',
+      ''
+    )
+    expect(replaceSpy).not.toHaveBeenCalledWith('-----END CERTIFICATE-----', '')
+    expect(replaceSpy).not.toHaveBeenCalledWith(
       '-----BEGIN ENCRYPTED PRIVATE KEY-----',
       ''
     )
-    expect(replaceSpy).toHaveBeenCalledWith(
+    expect(replaceSpy).not.toHaveBeenCalledWith(
       '-----END ENCRYPTED PRIVATE KEY-----',
       ''
     )
